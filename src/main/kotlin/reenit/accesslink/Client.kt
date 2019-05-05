c0bf4c48c3413b444adfa9c61a862e0e2f5dac91
Matias Keveri <keveri@users.noreply.github.com>
2019-05-05 19:02:10 +0300

    Get exercise data (#1)
    * Add exercise data type and request
    * Add user registration and deletion
    * Add separate gradle tasks for authorization and application
    * Add user info API call
    * Add transaction creation and fix exercise serialization

---
+++ src/main/kotlin/reenit/accesslink/Client.kt
@@ -0,0 +1,32 @@
+package reenit.accesslink
+
+import com.github.kittinunf.result.map
+import com.natpryce.konfig.*
+import java.io.File
+
+fun main(args: Array<String>) {
+    println("hello")
+    val user_id    = Key("user.id", intType)
+    val user_token = Key("user.token", stringType)
+    val config     = ConfigurationProperties.fromFile(File("user-info.properties"))
+    val userInfo   = UserInfo(config[user_id], config[user_token])
+
+    // 401: old token
+    // 200: created
+    //val userResult = register(userInfo)
+
+    val userInfoResult = info(userInfo)
+    userInfoResult.map { println(it) }
+
+    createTransaction(userInfo).map { println(it) }
+
+    listExercises(userInfo).fold({ value ->
+        println("Data:")
+        println(value.map { it.toString() })
+    }, { error ->
+        println("Error: $error")
+    })
+
+    //204: deleted
+    //delete(userInfo)
+}
---
+++ src/main/kotlin/reenit/accesslink/TrainingData.kt
@@ -0,0 +1,74 @@
+package reenit.accesslink
+
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.result.Result
+import com.github.kittinunf.fuel.core.FuelError
+import com.github.kittinunf.fuel.gson.responseObject
+import com.google.gson.annotations.SerializedName
+
+data class UserInfo(
+        val id: Int,
+        val token: String
+)
+
+data class HeartRate(
+        val average: Int,
+        val maximum: Int
+)
+
+data class Exercise(
+        val id: Int,
+        @SerializedName("upload-time")
+        val uploadTime: String,
+        @SerializedName("polar-user")
+        val polarUser: String,
+        val device: String,
+        @SerializedName("start-time")
+        val startTime: String,
+        val duration: String,
+        val calories: Int,
+        val distance: Float,
+        @SerializedName("heart-rate")
+        val heartRate: HeartRate,
+        @SerializedName("training-load")
+        val trainingLoad: Float,
+        val sport: String,
+        @SerializedName("has-route")
+        val hasRoute: Boolean,
+        @SerializedName("club-id")
+        val clubId: Int,
+        @SerializedName("club-name")
+        val clubName: String,
+        @SerializedName("detailed-sport-info")
+        val detailedSportInfo: String
+)
+
+typealias Exercises = Array<Exercise>
+
+data class TransactionLocation(
+        @SerializedName("transaction-id")
+        val transactionId: Int,
+        @SerializedName("resource-url")
+        val resourceUrl: String
+)
+
+fun createTransaction(userInfo: UserInfo): Result<TransactionLocation, FuelError> {
+    val path = "https://www.polaraccesslink.com/v3/users/${userInfo.id}/exercise-transactions"
+    val (_, response, result) =
+            Fuel.post(path)
+                    .appendHeader("Accept", "application/json;charset=UTF-8")
+                    .appendHeader("Authorization", "Basic ${userInfo.token}")
+                    .responseObject<TransactionLocation>()
+    println(response)
+    return result
+}
+
+fun listExercises(userInfo: UserInfo): Result<Exercises, FuelError> {
+    val (_, response, result) =
+            Fuel.get("https://www.polaraccesslink.com/v3/exercises")
+                    .appendHeader("Accept", "application/json;charset=UTF-8")
+                    .appendHeader("Authorization", "Basic ${userInfo.token}")
+                    .responseObject<Exercises>()
+    println(response)
+    return result
+}
