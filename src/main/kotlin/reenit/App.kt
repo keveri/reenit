7e52629c9ba42341df31d6da6dfe1351846eb887
Matias Keveri <keveri@users.noreply.github.com>
2019-05-26 20:31:00 +0300

    Add continuous integration (#3)
    * Add Travis CI configuration
    * Use travis cache with gradle
    * Add ktlint linter to build

--- src/main/kotlin/reenit/App.kt
+++ src/main/kotlin/reenit/App.kt
@@ -5,18 +5,21 @@ import com.github.kittinunf.fuel.core.FuelError
 import com.github.kittinunf.fuel.core.extensions.authentication
 import com.github.kittinunf.fuel.gson.responseObject
 import com.github.kittinunf.result.Result
-import com.natpryce.konfig.*
-import spark.kotlin.*
+import com.natpryce.konfig.ConfigurationProperties
+import com.natpryce.konfig.Key
+import com.natpryce.konfig.stringType
+import spark.kotlin.Http
+import spark.kotlin.ignite
 import java.io.File
 
 data class AccessTokenResponse(
-        val access_token: String,
-        val x_user_id: Int,
-        val token_type: String,
-        val expires_in: Int
+    val access_token: String,
+    val x_user_id: Int,
+    val token_type: String,
+    val expires_in: Int
 )
 
-fun getAccessToken(api_id: String, api_key:String, code: String): Result<AccessTokenResponse, FuelError> {
+fun getAccessToken(api_id: String, api_key: String, code: String): Result<AccessTokenResponse, FuelError> {
     val (_, _, result) =
             Fuel.post("https://polarremote.com/v2/oauth2/token")
                     .authentication()
@@ -29,9 +32,9 @@ fun getAccessToken(api_id: String, api_key:String, code: String): Result<AccessT
 }
 
 fun main(args: Array<String>) {
-    val api_id  = Key("api.id", stringType)
+    val api_id = Key("api.id", stringType)
     val api_key = Key("api.key", stringType)
-    val config  = ConfigurationProperties.fromResource("api.properties")
+    val config = ConfigurationProperties.fromResource("api.properties")
 
     val authUrl = "https://flow.polar.com/oauth2/authorization?response_type=code&client_id=${config[api_id]}"
     val http: Http = ignite()
@@ -40,7 +43,7 @@ fun main(args: Array<String>) {
     http.redirect.get("/", authUrl)
 
     http.get("/oauth2_callback") {
-        val code          = request.queryParams("code")
+        val code = request.queryParams("code")
         val tokenResponse = getAccessToken(config[api_id], config[api_key], code)
 
         tokenResponse.fold({ value ->
